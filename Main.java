f53158e21d7c3351dc0ec91f3b158e54bec75762
Vladimir <vaparfenov@hse.ru>
2024-10-25 10:47:13 +0300

    add writetofile private class

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
-import java.util.PriorityQueue;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
 
 public class Main {
@@ -16,7 +19,37 @@ public class Main {
         toyPool.add(toy3);
         toyPool.add(toy4);
         toyPool.add(toy5);
+
+        List<String> results = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Toy toy = toyPool.poll();
+//            System.out.println(toyPool.poll());
+            if (toy != null) {
+                results.add(toy.toString());
+//                System.out.println(toy.toString());
+            } else {
+//                System.out.println("Очередь пуста");
+                break;
+            }
+            results.add(toy.toString());
+//            System.out.println(results);
+        }
+
+        try {
+            writeToFile(results);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     
     }
-    
+     private static void writeToFile(List<String> data) throws IOException {
+        File file = new File("results.txt");
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (String line : data) {
+                writer.write(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
